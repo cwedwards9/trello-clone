63d1fc86a31e9951195b36a6f8fe700d5798dcd7
Chase Edwards <chase.w.edwards9@gmail.com>
2021-03-30 17:40:59 -0400

    Display a form for creating new lists. Add function for posting a new card for the list to the db. Display all of the list's Card components.

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "./Card";
+import useInputState from "../hooks/useInputState";
+import axios from "axios";
 import listStyles from "./List.module.scss";
 
-export default function Card(props) {
+export default function List(props) {
+    const [newCardDesc, updateCardDesc, resetCardDesc] = useInputState("");
+    const [listCards, setListCards] = useState(props.list.Cards || []);
+
+    const createCard = (e) => {
+        e.preventDefault();
+        axios.post("/api/cards", { description: newCardDesc, ListId: props.list.id })
+            .then((response) => {
+                setListCards([...listCards, response.data])
+                resetCardDesc();
+            })
+    }
+
     return (
         <section className={listStyles.list}>
-            {props.children}
+            <h3>{props.list.listTitle}</h3>
+            {listCards && listCards.map(card => (
+                <Card key={card.id} card={card} />
+            ))}
+
+            <form onSubmit={createCard} className={listStyles.newCardForm}>
+                <input value={newCardDesc} onChange={updateCardDesc} placeholder="+ Add another card" />
+                <button>Add Card</button>
+            </form>
         </section>
     );
 }
