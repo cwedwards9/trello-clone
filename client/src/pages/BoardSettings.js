d7bb0e8d906a42e41b133a6d1a1ac6bba6577af4
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-02 19:17:09 -0400

    Get boards from db, pass down to BoardEdit component. Add methods for deleting and updating a board.

--- client/src/pages/BoardSettings.js
+++ client/src/pages/BoardSettings.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import BoardEdit from "../components/BoardEdit";
+import axios from "axios";
+import settingsStyles from "./BoardSettings.module.scss";
 
-export default function ManageBoards() {
+export default function BoardSettings() {
+    const [boardsList, setBoardsList] = useState([]);
+
+    useEffect(() => {
+        axios.get("/api/boards")
+            .then(response => {
+                console.log(response.data.boardData)
+                setBoardsList(response.data.boardData);
+            })
+    }, []);
+
+    const deleteBoard = (id) => {
+        axios.delete(`/api/boards/${id}`)
+            .then(() => {
+                const updatedList = boardsList.filter(b => b.id !== id);
+                setBoardsList(updatedList);
+            })
+    }
+
+    const updateBoard = (id, updateBoard) => {
+        axios.put(`/api/boards/${id}`, updateBoard)
+            .then(() => {
+                const updatedList = boardsList.map(board => {
+                    if(board.id === id) {
+                        return {...board, ...updateBoard}
+                    }
+                    return board;
+                })
+                setBoardsList(updatedList);
+            });
+    }
+
     return (
-        <>
-        <h1>Board settings</h1>
-        
-        </>
+        <div className={settingsStyles.settingsContainer}>
+            <header className={settingsStyles.settingsHeader}>
+                <h1>Board Settings</h1>
+                <p>Manage your boards: edit or delete boards from existence.</p>
+                <hr />
+            </header>
+            <main className={settingsStyles.boardSettingsList}>
+                {boardsList.map(b => (
+                    <BoardEdit 
+                        boardInfo={b} 
+                        deleteBoard={deleteBoard}
+                        updateBoard={updateBoard}
+                    />
+                ))}
+            </main>
+        </div>
     );
 };
