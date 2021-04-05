a441e4b886da9292b86e376f43e52e3ba9922b59
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-05 18:38:23 -0400

    Initial commit - modal for list actions (update/delete/add to list).

---
+++ client/src/components/EditListModal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import useToggle from "../hooks/useToggle";
+import { ImCross } from "react-icons/im"
+import editListStyles from "./EditListModal.module.scss";
+
+export default function EditListModal(props) {
+    const [isDeleteMsg, toggleDeleteMsg] = useToggle(false);
+    
+    return (
+        <div className={editListStyles.listEditModal}>
+            <div>
+                <h3>List Actions</h3>
+                <button onClick={props.toggleListModal} className={editListStyles.listModalExit}><ImCross /></button>
+            </div>
+            <hr />
+            <ul>
+                <li onClick={function() {props.toggleListModal(); props.inputFocus();}}>Add card...</li>
+                <li onClick={function() {props.toggleListModal(); props.toggleEditListTitle();}}>Rename list...</li>
+                {isDeleteMsg ?
+                    <li>Confirm list deletion?
+                        <span className={editListStyles.deleteButtons}>
+                            <button onClick={props.handleDelete}>Yes</button>
+                            <button onClick={toggleDeleteMsg}>No</button>
+                        </span>
+                    </li>
+                    :
+                    <li onClick={toggleDeleteMsg}>Delete list...</li>
+                }
+            </ul>
+        </div>
+    );
+}
