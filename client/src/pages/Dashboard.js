989bd8c6f5b661e10323c7512eb6a6c8ffd97606
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-16 18:54:54 -0400

    Add get request to get all boards from db and set them to statethen loop through the boards state and display in the dashboard, displaying starred and all boards respectively.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import NewBoardModal from "../components/NewBoardModal";
 import useToggle from "../hooks/useToggle";
-import useInputState from "../hooks/useInputState";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import dashStyles from "./Dashboard.module.scss";
-
+import { HiPlusSm } from 'react-icons/hi';
+import { RiTrelloFill } from 'react-icons/ri';
+import { BsStar } from 'react-icons/bs';
+import { IoSettingsSharp } from 'react-icons/io5';
 
 export default function Dashboard() {
     const [modal, toggleModal] = useToggle(false);
-    const [boardName, updateName] = useInputState("");
-    const [boardDesc, updateDesc] = useInputState("");
+    const [boardsList, setBoardsList] = useState([]);
 
-    const createBoard = (e) => {
-        e.preventDefault();
+    useEffect(() => {
+        axios.get("/api/boards")
+            .then(response => {
+                setBoardsList(response.data.boardData);
+            })
+    }, [setBoardsList]);
 
-        axios.post("/test", {name: boardName, description: boardDesc })
-            .then(() => {
-                updateName("");
-                updateDesc("");
+    const createBoard = (boardInfo) => {
+        axios.post("/api/boards", boardInfo)
+            .then((response) => {
+                setBoardsList([...boardsList, response.data]);
             })
     }
 
@@ -27,43 +33,52 @@ export default function Dashboard() {
                 <header>
                     <h1>Trellolo Boards</h1>
                     <p>Get started with a new board or continue working in an existing board.</p>
+                    <button className={dashStyles.createBoardMainBtn} onClick={toggleModal}>Create Board</button>
                     <hr />
                 </header>
 
                 <section>
-                    <button onClick={toggleModal}>➕ Create Board</button>
+                    <h2><BsStar /> Starred Boards</h2>
+                    <div className={dashStyles.dashboardBoards}>
+                    {boardsList && boardsList.map(d => (
+                        d.isStarred ?
+                        <div key={d.id} style={{backgroundImage: `url(${d.bgImage})`}}>
+                            <Link to={`/board/${d.id}`} className={dashStyles.boardLink}>{d.name}</Link>
+                        </div>
+                        : null
+                    ))}
+                    </div>
                 </section>
 
                 <section>
-                    <h2>⭐ Starred Boards</h2>
-                </section>
-
-                <section>
-                    <h2>💻 All Boards</h2>
+                    <h2><RiTrelloFill /> All Boards</h2>
+                    <div className={dashStyles.dashboardBoards}>
+                    {boardsList.map(d => (
+                        <div key={d.id} style={{backgroundImage: `url(${d.bgImage})`}}>
+                            <Link to={`/board/${d.id}`} className={dashStyles.boardLink}>{d.name}</Link>
+                        </div>
+                    ))}
+                        <button onClick={toggleModal} className={dashStyles.createBoardBtn}><HiPlusSm /> Create Board</button>
+                    </div>
                 </section>
             </main>
+
             <aside className={dashStyles.sidebar}>
                 <ul>
                     <div>
-                        <li><Link to="/dashboard" className={dashStyles.sideLink}>💻 Boards</Link></li>
-                        <li className={dashStyles.sideLink}><button onClick={toggleModal}>➕ Create Board</button></li>
+                        <li><Link to="/dashboard" className={dashStyles.sideLink}><RiTrelloFill /> Boards</Link></li>
+                        <li className={dashStyles.sideLink}><button onClick={toggleModal}><HiPlusSm /> Create Board</button></li>
                     </div>
-                    <li><Link to="/board-settings" className={dashStyles.sideLink}>⚙ Board Settings</Link></li>
+                    <li><Link to="/board-settings" className={dashStyles.sideLink}><IoSettingsSharp /> Board Settings</Link></li>
                 </ul>
             </aside>
 
             {modal ? 
-                <div className={dashStyles.newBoardModal}>
-                    <h2>Create New Board</h2>
-                    <form onSubmit={createBoard}>
-                        <label>Board Name</label>
-                        <input type="text" value={boardName} onChange={updateName} />
-                        <label>Description</label>
-                        <input type="text" value={boardDesc} onChange={updateDesc} />
-                        <button>Create</button>
-                        <button onClick={toggleModal}>Cancel</button>
-                    </form>
-                </div>
+                <NewBoardModal
+                    toggleModal={toggleModal}
+                    createBoard={createBoard}
+                    
+                />
                 : null
             }
         </div>
