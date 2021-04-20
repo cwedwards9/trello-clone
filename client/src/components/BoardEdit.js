a9470e4dce460704078cb4fc45d9ca708adf9710
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-20 11:02:33 -0400

    Add a modal for confirming board deletion. Add background image styling to each card.

--- client/src/components/BoardEdit.js
+++ client/src/components/BoardEdit.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import useToggle from "../hooks/useToggle";
 import useInputState from "../hooks/useInputState";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import boardEditStyles from "./BoardEdit.module.scss";
+import dateFormat from "dateformat";
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { BiPencil, BiTrash } from 'react-icons/bi';
 
 export default function BoardEdit(props) {
     const [isEditingBoard, toggleEditForm] = useToggle(false);
-    const { id, name, description, isStarred, updatedAt } = props.boardInfo;
+    const { id, name, description, isStarred, bgImage, updatedAt } = props.boardInfo;
     const [boardName, updateBoardName] = useInputState(name);
     const [boardDesc, updateBoardDesc] = useInputState(description);
+    const [isModal, toggleModal] = useToggle(false);
 
     const handleUpdate = (e) => {
         e.preventDefault();
@@ -22,8 +23,9 @@ export default function BoardEdit(props) {
 
 
     return (
-        isEditingBoard ? 
-            <form onSubmit={handleUpdate} className={boardEditStyles.boardEditForm}>
+        <>
+        {isEditingBoard ? 
+            <form onSubmit={handleUpdate} className={boardEditStyles.boardEditForm} style={{backgroundImage: `url(${bgImage})`}}>
                 <div>
                     <label>Board Name:</label>
                     <input value={boardName} onChange={updateBoardName} />
@@ -36,20 +38,37 @@ export default function BoardEdit(props) {
                 <button onClick={toggleEditForm}>Cancel</button>
             </form>
         :
-            <section key={id} className={boardEditStyles.boardSettingsCard}>
+            <section key={id} className={boardEditStyles.boardSettingsCard} style={{backgroundImage: `url(${bgImage})`}}>
                 <div className={boardEditStyles.boardSettingsTitle}>
                     <h2><Link to={`/board/${id}`}>{name}</Link></h2>
                     { isStarred ? <button style={{ color: "gold"}}><AiFillStar /></button> : <button><AiOutlineStar /></button> }
                 </div>
                 <div className={boardEditStyles.boardSettingsBtns}>
                     <button onClick={toggleEditForm}><BiPencil /></button>
-                    <button onClick={() => props.deleteBoard(id)}><BiTrash /></button>
+                    <button onClick={toggleModal}><BiTrash /></button>
                 </div>
                 <div className={boardEditStyles.boardSettingsInfo}>
                     <p>{description}</p>
-                    
-                    <p>{updatedAt}</p>
+                    <p>Last updated: {dateFormat(updatedAt, "fullDate")}</p>
                 </div>
             </section>
+        }
+
+        {isModal
+            ?
+                <div className={boardEditStyles.deleteBoardModal}>
+                    <h3>Delete Board?</h3>
+                    <hr />
+                    <p>Are you sure you want to delete the project "{name}"?</p>
+                    <div>
+                        <button onClick={() => props.deleteBoard(id)} className={boardEditStyles.deleteBtn}>Delete</button>
+                        <button onClick={toggleModal} className={boardEditStyles.cancelBtn}>Cancel</button>
+                    </div>
+                </div>
+            :
+                null
+        }
+            
+        </>
     );
 }
