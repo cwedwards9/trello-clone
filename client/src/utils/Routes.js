03829c02f216e99dfb3dd749a34ef7cc0d1571b5
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-23 13:49:27 -0400

    Add routes for signup and login.

--- client/src/utils/Routes.js
+++ client/src/utils/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "../pages/Home";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import Board from "../pages/Board";
 import BoardSettings from "../pages/BoardSettings";
@@ -10,6 +12,8 @@ export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" render={ () => <Home /> } />
+            <Route exact path="/signup" render={ () => <Signup /> } />
+            <Route exact path="/login" render={ () => <Login /> } />
             <Route exact path="/dashboard" render={ () => <Dashboard /> } />
             <Route exact path="/board/:id" render={ () => <Board /> } />
             <Route exact path="/board-settings" render={ () => <BoardSettings /> } />
