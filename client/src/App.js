384e85a71c6850aab37693ce3bf4ef7778762e60
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-29 18:43:00 -0400

    Remove navbar component (to prevent it showing up on every page).

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Routes from "./utils/Routes";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes />
     </div>
   );
