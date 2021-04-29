67dbca50c9698f551932f002db9e572933e58e38
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-29 19:10:25 -0400

    On render, get 'user' from session storage and if there is session conditionally render /logout and /dashboard links otherwise render /login and /signup links. Add a method for logging a user out of session (server) and session storage (browser).

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.scss";
+import { ImTrello } from "react-icons/im";
+import { BiLogIn } from "react-icons/bi";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoPersonAddOutline } from "react-icons/io5";
+import { RiDashboardLine } from "react-icons/ri";
+import axios from "axios";
 
 export default function Navbar() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const user = sessionStorage.getItem("user");
+        if(user) setIsLoggedIn(true);
+        else setIsLoggedIn(false);
+    });
+
+    const handleLogout = async () => {
+        await axios.get("/api/logout");
+        setIsLoggedIn(false);
+        sessionStorage.clear();
+        window.location.replace("/");
+    }
 
     if(isLoggedIn) {
         return (
             <nav className={styles.navbar}>
-                <NavLink exact to="/" className={styles.navLink}>Trellolo</NavLink>
+                <NavLink exact to="/" className={`${styles.navLink} ${styles.brandName}`}><ImTrello /> Trellolo</NavLink>
                 <ul>
                     <li>
-                        <NavLink exact to="/" activeClassName={styles.activeLink} className={styles.navLink}>Home</NavLink>
+                        <NavLink exact to="/dashboard" className={styles.navLink}><RiDashboardLine className={styles.icon} /> Dashboard</NavLink>
                     </li>
                     <li>
-                        <NavLink exact to="/dashboard" activeClassName={styles.activeLink} className={styles.navLink}>Dashboard</NavLink>
+                        <button type="button" className={styles.logoutBtn} onClick={handleLogout}><FaSignOutAlt className={styles.icon} /> Log Out</button>
                     </li>
                 </ul>
             </nav>
@@ -23,13 +42,13 @@ export default function Navbar() {
     else {
         return (
             <nav className={styles.navbar}>
-                <NavLink exact to="/" className={styles.navLink}>Trello Clone</NavLink>
+                <NavLink exact to="/" className={`${styles.navLink} ${styles.brandName}`}><ImTrello /> Trellolo</NavLink>
                 <ul>
                     <li>
-                        <NavLink exact to="/" activeClassName={styles.activeLink} className={styles.navLink}>Log In</NavLink>
+                        <NavLink exact to="/login" activeClassName={styles.activeLink} className={styles.navLink}><BiLogIn className={styles.icon} /> Log In</NavLink>
                     </li>
                     <li>
-                        <NavLink exact to="/dashboard" activeClassName={styles.activeLink} className={styles.navLink}>Sign Up</NavLink>
+                        <NavLink exact to="/signup" activeClassName={styles.activeLink} className={styles.navLink}><IoPersonAddOutline className={styles.icon} /> Sign Up</NavLink>
                     </li>
                 </ul>
             </nav>
