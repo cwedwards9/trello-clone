ac65047d0f48221fb822e0882249e98aac5c9057
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-29 19:04:00 -0400

    Update footer, add 'links' and copyright notice.

---
+++ client/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import footerStyles from "./Footer.module.scss";
+
+export default function Footer() {
+    const links = ["Templates", "Pricing", "Apps", "Jobs", "Blog", "Developers", "About", "Help", "Legal", "Cookie Settings", "Privacy"];
+    return (
+        <footer className={footerStyles.footer}>
+            <select>
+                <option>Light Mode</option>
+                <option>Dark Mode (Coming Soon)</option>
+            </select>
+            <ul>
+                {links.map(link => (
+                    <li key={link}>{link}</li>
+                ))}
+            </ul>
+            <p class={footerStyles.copyright}>
+                © Copyright 2021. All rights reserved. Created by Chase.
+            </p>
+        </footer>
+    );
+}
